2649129973fc6e1b082b07c67198af9238426030
Saqib Ali <saqibali.fd@gmail.com>
2025-09-12 13:25:23 +0500

    order placing error solved

--- lib/service/orders_services.dart
+++ lib/service/orders_services.dart
@@ -9,12 +9,25 @@ class OrderService {
     required String address,
     required List<Map<String, dynamic>> cartItems,
   }) async {
+    final String userId = Supabase.instance.client.auth.currentUser!.id;
+    // final OrderModel orderModel = OrderModel(
+    //   id: 38923434,
+    //   userId: int.parse(userId),
+    //   status: 'pending',
+    //   username: username,
+    //   email: email,
+    //   address: address,
+    //   products: ,
+    //   createdAt: DateTime.now().toIso8601String(),
+    // );
+
     final response = await Supabase.instance.client.from('myorders').insert({
       'username': username,
       'email': email,
       'address': address,
       'products': cartItems,
       'status': 'pending',
+      'userId': userId,
       'created_at': DateTime.now().toIso8601String(),
     }).select();
 
