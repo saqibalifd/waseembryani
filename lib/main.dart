0e6d286f91c2cca33aecbfa3fd31e1f2cca9c467
Saqib Ali <saqibali.fd@gmail.com>
2026-04-17 11:54:11 +0500

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:waseembrayani/pages/auth/login_screen.dart';
-import 'package:waseembrayani/pages/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
